6e6686892c66c634ff46f2fa98c443c86b2f64e1
masterccc <35172328+masterccc@users.noreply.github.com>
2018-02-20 23:38:32 +0100

    test - changement workspace

--- tests/i3_ipc_test.c
+++ tests/i3_ipc_test.c
@@ -10,21 +10,25 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define MSG_HEADER_SIZE 14
+#define HEADER_IPC "i3-ipc"
 #define TO_ASCII(x) (x + 48)
 
 void getSockPath(char *out);
 void create_ipc_fd(char *sockpath, int *sock);
-void send_change_workspace(int sock, int ws);
-
+void send_change_workspace(int sock);
 
 int main (void){
 
-    int sock ;
+    int sock, msize ;
     char out[50] = {0};
     getSockPath(out);
     printf("out : <%s>", out);
     create_ipc_fd(out, &sock);
-    send_change_workspace(sock,5);
+    
+    send_change_workspace(sock);
+
+    return 0;
 
 }
 
@@ -36,38 +40,64 @@ void create_ipc_fd(char *sockpath, int *sock){
    
     if (*sock == -1) {
         puts("Socket() error");
-        return -1;
     }
     else {
         printf("Socket created...");
-    }
+    
 
-    memset(&addr, 0, sizeof(struct sockaddr_un));
-    addr.sun_family = AF_LOCAL;
-    strncpy(addr.sun_path, sockpath, sizeof(addr.sun_path) - 1);
+        memset(&addr, 0, sizeof(struct sockaddr_un));
+        addr.sun_family = AF_LOCAL;
+        strncpy(addr.sun_path, sockpath, sizeof(addr.sun_path) - 1);
 
-    if (connect(*sock, (const struct sockaddr*)&addr, sizeof(struct sockaddr_un)) < 0) {
-        puts("connect error");
-        return -1;
-    }
-    else {
-        printf("Socket connected...");
+        if (connect(*sock, (const struct sockaddr*)&addr, sizeof(struct sockaddr_un)) < 0) {
+            puts("connect error");
+        }
+        else {
+            printf("Socket connected...");
+        }
     }
 
 }
 
-void send_change_workspace(int sock, int ws){
 
-    /* exit i3 payload */
-    /* char msg[] = { 0x69,0x33,0x2d,0x69,0x70,0x63,0x0D,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x65,0x78,0x69,0x74 } ;*/
+char* forge_ipc_message(char type, char payload_size, char *payload){
+
+    int i ;
+    char *msg = malloc((MSG_HEADER_SIZE + payload_size) * sizeof(char));
+    char *p = msg ;
+
+    memcpy(p, HEADER_IPC, strlen(HEADER_IPC));
+    p += strlen(HEADER_IPC);
+    *p++ = payload_size ;
     
+    for(i=0;i<7;i++){
+        *p++ = 0 ;
+    }
+
+    memcpy(p, payload, payload_size);
+
+    return msg ;
+}
+
+
+
+void send_change_workspace(int sock){
+
+    char *msg = forge_ipc_message((char)0, (char)11, "workspace 5");
     int i,ret;
     char buffer[1024];
 
-    /* goto workspace 2 */
-    char msg[] = { 0x69,0x33,0x2d,0x69,0x70,0x63,0x0B,0x00,0x00,0x00,0x00,0x00,0x00,0x00, 0x77,0x6f,0x72,0x6b,0x73,0x70,0x61,0x63,0x65,0x20, TO_ASCII(ws) } ;
-
-    ret = send(sock, msg,sizeof(msg),MSG_CONFIRM);
+/* 
+    Go to workspace 5
+    cmd : "workspace 5"
+    char msg[] = {
+        0x69,0x33,0x2d,0x69,0x70,
+        0x63,0x0B,0x00,0x00,0x00,
+        0x00,0x00,0x00,0x00,0x77,
+        0x6f,0x72,0x6b,0x73,0x70,
+        0x61,0x63,0x65,0x20,0x35 };
+*/
+    ret = send(sock, msg, MSG_HEADER_SIZE + 11,MSG_CONFIRM);
 
     if(ret <= 0)
         printf("Error (send) : %d", ret);
@@ -77,8 +107,9 @@ void send_change_workspace(int sock, int ws){
         printf("%s", buffer);    
     }
 
-}
+    free(msg);
 
+}
 
 void getSockPath(char *out){
 
